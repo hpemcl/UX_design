db4b114ce50a387c674c6bf35813107542b735b0
hannahbanana <hannahdk00@gmail.com>
2022-02-21 14:57:13 +0100

    fra hannahs' bærbar til pc - opdatering med alles koder

---
+++ reservescript.js
@@ -0,0 +1,14 @@
+// https://codepen.io/chabzz/pen/mVYVpY
+
+$ = function(id) {
+    return document.getElementById(id);
+  }
+  
+  var show = function(id) {
+      $(id).style.display ='block';
+      // setTimeout( function() {
+      //   $(id).style.display ='none'}, 1500);
+  }
+  var hide = function(id) {
+      $(id).style.display ='none';
+  }
